4c08304912f9b386bc498a99498a048e0147ceaf
jacksparrow200207 <zuhriddin02b@gmail.com>
2023-02-21 20:39:39 +0500

    test

---
+++ Java script/index.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+
+let a=1;
+let b=2;
+console.log(typeof a)
---
+++ videolar uchun/index.js
@@ -0,0 +1,45 @@
+// Data type => malumot turlari
+// 8 ta data type bor => 2 ga bolinadi => primitive vs non-primitive
+
+// primitive
+
+// number => bu son
+// string => harf, text
+// boolean => true, false
+// null =>e'lon qilingan qiymat biriktirilgan o'zgaruvchi kk bolmay qolsa ochirib qoyish uchun  
+// undefined => o'zgaruvchi e'lon qilingan ammo qiymat berilmagan
+// bigInt => katta sonlar bilan ishlaganda
+// symbol => id larni saqlash uchun ishlatiladi
+
+// non-primitive => object(array, function,object);
+
+
+// variables
+
+// Js uzgaruvchi yaratish uchun  3 ta keyword mavjud 
+// 1- var,  2- let, 3-const ES-(EcmaScript)
+
+// var => qiymatni uzgartira olamiz va usha bir xil uzgaruvchi nomi bilan qaytadan elon qilolamiz,housting boladi
+// let -> qiymatni uzgartirolamiz va bir xil nom bilan qayta uzgaruvchi yarata olmaymiz,housting bolmaydi
+// const(uzgarmas) -> qiymatniyam uzgartirolmimiz va qaytadan bir xil nom bilan elon qilolmaymiz housting bolmaydi
+
+//==,===
+
+
+//Number, not a number NaN Infinity
+
+
+//''  ,  ""  , ``
+
+let a=40
+
+console.log(isNaN(a+1));
+
+
+//
+
+
+
+
+ 
+ 
