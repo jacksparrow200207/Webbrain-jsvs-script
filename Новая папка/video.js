2d0fb72d0d853ad86dc263184b2ef0220ceb5629
jacksparrow200207 <zuhriddin02b@gmail.com>
2023-03-04 16:31:56 +0500

    switch case, for loop, while , do while

---
+++ Новая папка/video.js
@@ -0,0 +1,29 @@
+//For loop 
+
+//for (i=1; i<=10; i++){
+ // for (let j=1; j<=10;j++){console.log(i ,"(i)", "*" , j , "(j)" , "=" , i*j);}}
+
+ //  while(i<=10){console.log(i);
+ // i++
+ // }
+ //let i=1;
+
+// do {
+//   console.log(i);
+//   i++
+// }  while (i=0)
+
+
+
+switch ("Sevinch"){
+case "Zuhriddin": 
+     alert("Ushbu ism Ogil bollarga tegishli");
+     break;
+case "Sevinch" : 
+alert("Ushbu ism qizlarga tegishli");
+break;
+
+default: 
+alert("Bu ism Ozbek ismi emas");
+
+}
