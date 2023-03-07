631b558bd7b8c22792c1dc6547d557b4bd08cea1
jacksparrow200207 <zuhriddin02b@gmail.com>
2023-03-07 14:25:30 +0500

    function

---
+++ function/Function1.js
@@ -0,0 +1,50 @@
+// let a=8;
+// let b=8;
+// function smaller() {
+//   if (a>b) {
+//     console.log(b);
+//   } else if (b>a) {
+//     console.log(a);
+//   } else {
+//     console.log("Equal");
+//   }
+  
+// }
+
+// smaller()
+
+
+
+//======================
+
+// let age=17;
+// function checkAge() {
+//   if (age>18){
+//     return true;
+//   } else {
+//      return "Did parents allow you?";
+//   }
+ 
+  
+// }
+
+
+// console.log(checkAge());
+
+//=============================
+
+ 
+ 
+  
+
+
+
+// console.log(checkAge());
+//============================
+
+
+// console.log(Math.max(8,5));
+
+//=============================
+
+// console.log(Math.pow(2,3));
---
+++ function/function.js
@@ -0,0 +1,23 @@
+
+// function func() {
+//   console.log("Assalomu alaykum java script function qismiga hush kelibsiz");
+// }
+
+
+
+
+//   func();
+
+function genr(name , age , level , marriage) {
+  console.log( `Name= ${name}` );
+  console.log( `Age= ${age}` );
+  console.log( `Level= ${level}` );
+  console.log( `Marriage= ${marriage}` );
+  console.log( `==============` );
+  
+}
+
+genr( "Zuhriddin" , 20 , "Bachelor" , "Single");
+genr( "Umar" , 20 , "IT specialist" , "Single");
+genr( "Zayriddin" , 20 , "Bachelor" , "Single");
+genr( "Diyor" , 20 , "Bachelor" , "Single");
