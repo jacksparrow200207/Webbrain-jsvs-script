d7027c81bd75e5c3d73953f744cab7d79b3614c1
jacksparrow200207 <zuhriddin02b@gmail.com>
2023-03-27 14:32:12 +0500

    algorithms

---
+++ CRUD/index.js
@@ -0,0 +1,19 @@
+// let user = [1, 8, 7, 4, 5, 15, 14, 75, 54, 36, 10, 47]
+
+
+let task = "Webbrain Academy"
+
+
+const getquantity = () => {
+  res = 0
+  count = 0
+  for(i of task){
+    if(i===i.toUpperCase()){
+      res+= 1
+    }else return count+= 
+  }
+}
+
+console.log(getquantity());;
+
+// console.log(task);
